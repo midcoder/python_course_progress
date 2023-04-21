9a3ef4efe7f32122ec9a89f7e5a2438d9524063f
Alexis Rocha <111248182+midcoder@users.noreply.github.com>
2023-04-21 10:55:57 -0700

    Add files via upload
    Final score represented with letters program

---
+++ ex_03_02.py
@@ -0,0 +1,12 @@
+s = input('Enter score: ')
+ss = float(s)
+if ss >= .9 :
+    print('A')
+elif ss >= .8 :
+    print('B')
+elif ss >= .7 :
+    print('C')
+elif ss >= .6 :
+    print('D')
+elif ss < .6 :
+    print('F')
